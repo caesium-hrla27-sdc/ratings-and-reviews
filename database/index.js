a1172de027b8c22745b855330b332ad2d0972b23
Jennifer Haren <jennifer.haren110@gmail.com>
2019-03-01 20:10:21 -0800

    Modify index file
    Delete extraneous data.

---
+++ database/index.js
@@ -0,0 +1,33 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/reviews');
+
+var reviewSchema = new mongoose.Schema({
+    username: String,
+    ratingsNumber: Number, 
+    eyeColor: String, 
+    hairColor: String,
+    skinTone: String,
+    skinType: String,
+    ageRange: String,
+    skinConcerns: String,
+    notHelpfulCount: Number,
+    helpfulCount: Number,
+    date: Date,
+    review: String
+})
+
+var Review = mongoose.model('Review', reviewSchema);
+
+var productSchema = new mongoose.Schema({
+  id: Number,
+  productName: String,
+  fiveStarReviews: [reviewSchema],
+  fourStarReviews: [reviewSchema],
+  threeStarReviews: [reviewSchema],
+  twoStarReviews: [reviewSchema],
+  oneStarReviews: [reviewSchema]
+})
+
+var Product = mongoose.model('Product', productSchema);
+
+module.exports = { Review, Product };
