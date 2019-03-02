fbb435d8ba0cab1850e8eb71ec86c90bc7acca89
Jennifer Haren <jennifer.haren110@gmail.com>
2019-03-01 20:00:11 -0800

    Modify getRatings function to log time in ms
    Modified function to log time in milliseconds between invoking function to query the database by product id, and after the query has ended.

--- server/index.js
+++ server/index.js
@@ -4,25 +4,41 @@ const path = require('path');
 const morgan = require('morgan');
 const PORT = 3003;
 const { Review, Product } = require('../database');
-const generateData = require('./generateData');
+// const generateData = require('./generateData');
 
 const app = express();
 
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
 
-Product.find().then(result => {
-  if (result.length === 0) {
-    generateData();
-  }
-});
+// This function will retrieve
+
+// Product.find().then(result => {
+//   if (result.length === 0) {
+//     generateData();
+//   }
+// });
+
+// const getRatings = (req, res) => {
+//   let { id } = req.params;
+//   Product.findOne({ id }).then(result => {
+//     res.status(200).json(result);
+//   });
+// };
 
 const getRatings = (req, res) => {
-  let { id } = req.params;
-  Product.findOne({ id }).then(result => {
-    res.status(200).json(result);
-  });
+  let {id} = req.params;
+
+  console.log('IN GET---------');
+  console.time('testBeg');
+  // let id = 10;
+  Product.find({id: id})
+    .then(result => {
+      console.timeEnd('testBeg');
+      res.status(200).json(result);
+    })
+
 };
 
 app.get('/ratings/:id', getRatings);
