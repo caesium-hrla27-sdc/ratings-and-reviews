17968ce573acace5ec4b6fc8f23110d3a6cc37d9
Jennifer Haren <jennifer.haren110@gmail.com>
2019-03-04 14:07:53 -0800

    Decided to refactor postgres model

--- server/generateMongoData.js
+++ server/generateMongoData.js
@@ -20,7 +20,7 @@ const randomizeArr = (arr) => {
 }
 
 var generateData = function (id) {
-  let newProduct
+  let newProduct = '';
   let productName = faker.lorem.words();
 
   let fiveStarReviews =[];
@@ -145,7 +145,7 @@ var generateData = function (id) {
 
   }
 
-  let newDocument = { productName, fiveStarReviews, fourStarReviews, threeStarReviews, twoStarReviews, oneStarReviews };
+  let newProduct += `${productName}, ${fiveStarReviews}, ${fourStarReviews}, ${threeStarReviews}, twoStarReviews, oneStarReviews `;
   
   return JSON.stringify(newDocument);
 }
